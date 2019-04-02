f35f53d7543f35e901c40bd41696f1ed19939a8f
dev1 <I LOVE MY FAMILY@DESKTOP-P6IDCMT>
2019-04-02 08:42:06 +0530

    commit by ved

---
+++ GITProj1/src/com/nit/dao/EmpDAO.java
@@ -0,0 +1,8 @@
+package com.nit.dao;
+
+import java.sql.SQLException;
+
+public interface EmpDAO {
+	public String getEmp()throws SQLException;
+
+}
---
+++ GITProj1/src/com/nit/dao/EmpDAOImpl.java
@@ -0,0 +1,18 @@
+package com.nit.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.nit.util.JdbcUtil;
+
+public class EmpDAOImpl implements EmpDAO {
+
+	@Override
+	public String getEmp() throws SQLException {
+		// TODO Auto-generated method stub
+		Connection util=null;
+		util=JdbcUtil.getConnection("oracle:thin:@localhost:1521:xe", "system", "system");
+		return "Connection established";
+	}
+
+}
